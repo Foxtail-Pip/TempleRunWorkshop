6415c03832c84fa0e8653636b5e6d60d11fee8b5
Foxtail-Pip <ren.isaac.green@gmail.com>
2025-10-02 11:54:11 +0100

    Changed to new input system

--- Assets/TempleRun_Pack/Scripts/PlayerScript.cs
+++ Assets/TempleRun_Pack/Scripts/PlayerScript.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-
+    [SerializeField] float laneChangeSpeed = 2f;
     [SerializeField] private int lane = 0;
     [SerializeField] private float laneWidth = 2f;
 
@@ -12,7 +13,11 @@ public class PlayerScript : MonoBehaviour
     {
         transform.Translate(0f, 0f, speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Lerp(pos.x, lane * laneWidth, Time.deltaTime * laneChangeSpeed);
+        transform.position = pos;
+
+      /*  if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             lane -= 1;
         }
@@ -24,9 +29,19 @@ public class PlayerScript : MonoBehaviour
 
         lane = Mathf.Clamp(lane, -1, 1);
 
-        transform.position = new Vector3(laneWidth * lane, transform.position.y, transform.position.z);
+        transform.position = new Vector3(laneWidth * lane, transform.position.y, transform.position.z); */
 
-        /* Oooohh comment ooohh
-         Wooooaah */
+    } 
+
+    public void ChangeLane(InputAction.CallbackContext inputData)
+    {
+        Vector2 inputVector = inputData.ReadValue<Vector2>();
+
+        Debug.Log(inputVector.x);
+
+        if (inputVector.x != 0)
+        {
+            lane = Mathf.Clamp(lane + Mathf.RoundToInt(inputVector.x), -1, 1);
+        }
     }
 }
